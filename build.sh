b7ec7ef9a58e7f0c6081bf8f785e9735d7ea91cd
Ben <benknight135@gmail.com>
2021-01-06 18:12:35 +0000

    organise code & build library as shared for use externally

--- build.sh
+++ build.sh
@@ -20,27 +20,33 @@ cd $SCRIPTPATH/Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd $SCRIPTPATH
 
+install_folder=$SCRIPTPATH/install
+
 echo "Configuring and building ORB_SLAM3 ..."
 mkdir build
+mkdir $install_folder
 cd build
 
 if [[ "$OSTYPE" == "msys" ]]; then
     # Setup CMake build
     cmake \
         -D CMAKE_BUILD_TYPE=RELEASE \
+        -D CMAKE_INSTALL_PREFIX=$install_folder \
         -D OpenCV_DIR=$opencv_dir \
         -D BOOST_ROOT=$boost_root \
         -D Eigen3_DIR=$eigen_root \
         -D Pangolin_DIR=$pangolin_dir ..
     # Build
-    cmake --build . --config Release --parallel $((`nproc`)) -j $((`nproc`))
+    cmake --build . --config Release --target install --parallel $((`nproc`)) -j $((`nproc`))
 else
     # Setup CMake build
     cmake $cmake_options \
         -D CMAKE_BUILD_TYPE=RELEASE \
+        -D CMAKE_INSTALL_PREFIX=$install_folder \
         -D OpenCV_DIR=$opencv_dir \
         -D Eigen3_DIR=$eigen_root \
         -D Pangolin_DIR=$pangolin_dir ..
     # Build
     make -j$((`nproc`+1))
+    make install
 fi
